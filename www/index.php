66d47d97dc6c779e8042434480c3b348c42ec4b5
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-06-29 12:47:29 +0200

    index.php

--- www/index.php
+++ www/index.php
@@ -8,10 +8,21 @@
     <title>Restaurantes</title>
 </head>
 <body>
-    
+    <header>
+        <div class="logo"></div>
+        <h1>Jalaria</h1>
+    </header>
+    <main>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+</div>
+    </main>
         
-            <img src="img/logos/9ca707d3-343f-48f6-8f7e-909255b59985_200x200.png" alt="" class="logo">
-            <h1>Jalaria</h1>
     
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
