4c401dc9ad53a88a971924860b985545c92ad158
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-06-30 13:47:23 +0200

    buscador

---
+++ www/componentes/buscador.php
@@ -0,0 +1,13 @@
+<?php require 'back/buscadorb.php' ?>
+<form action="" name="busqueda" class="buscar" method = "GET">
+        <input type="text" name ="busqueda" placeholder ="Buscar">
+        <button type="submit" class="icono fa fa-search" ></button>
+        <select name="OS">
+                <option value="1">Windows Vista</option> 
+                <option value="2">Windows 7</option> 
+                <option value="3">Windows XP</option>
+                <option value="10">Fedora</option> 
+                <option value="11">Debian</option> 
+                option value="12">Suse</option> 
+        </select>
+</form>
