8e28441194ad6ed0677e9d114d5a4d3070733329
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-06-30 11:49:21 +0200

    index

--- www/index.php
+++ www/index.php
@@ -9,14 +9,14 @@
         while($row = mysqli_fetch_array($result)){
 ?>
 
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $row['nombre']?></h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Ir al restaurante</a>
-    </div>
-    </div>
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title"><a href="restaurant.php?id=<?php echo $row['Id']; ?>"><?php echo $row['nombre']?></a></h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="restaurant.php?id=<?php echo $row['Id']; ?>" class="btn btn-primary">Ir al restaurante</a>
+        </div>
+        </div>
 
 <?php }}}?>
 
