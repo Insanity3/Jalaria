7343b003686ef8ae57bea087713382a02afec138
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-01 10:51:20 +0200

    single

--- www/restaurant.php
+++ www/restaurant.php
@@ -1,4 +1,17 @@
 <?php require 'back/restaurantb.php';?>
+<?php require 'componentes/header.php'?>
 
-    <h3 class="center"><?php echo $row['nombre']?></h3>
+    <h3><?php echo $row['nombre']?></h3>
+    <p><?php echo $row['localidad']?></p>
+    <p><?php echo $row['diceccion']?></p>
+    <p><?php echo $row['cp']?></p>
+    <p><?php echo $row['telf']?></p>
+    <p><?php echo $row['precios']?></p>
+    <p><?php echo $row['valoracion']?></p>
+    <p><?php echo $row['email']?></p>
+    <p><?php echo $row['web']?></p>
+    <p><?php echo $row['horario']?></p>
+    <p><?php echo $row['cocina']?></p>
                 
+
+<?php require 'componentes/footer.php'?>
