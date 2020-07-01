644a226c43f38420f0d3e0d9f1e38ec400e45f1e
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-01 10:25:53 +0200

    buscador

--- www/componentes/buscador.php
+++ www/componentes/buscador.php
@@ -1,13 +1,21 @@
 <?php require 'back/buscadorb.php' ?>
 <form action="" name="busqueda" class="buscar" method = "GET">
-        <input type="text" name ="busqueda" placeholder ="Buscar">
-        <button type="submit" class="icono fa fa-search" ></button>
-        <select name="OS">
-                <option value="1">Windows Vista</option> 
-                <option value="2">Windows 7</option> 
-                <option value="3">Windows XP</option>
-                <option value="10">Fedora</option> 
-                <option value="11">Debian</option> 
-                option value="12">Suse</option> 
+        <input type="text" name ="nombre" placeholder ="Ca na Carmen">
+        <input type="text" name ="localidad" placeholder ="Palma de Mallorca">
+        
+        <select name="precio">
+                <option value="1">1€</option> 
+                <option value="2">2€</option> 
+                <option value="3">3€</option>
+                <option value="4">4€</option> 
+                <option value="5">5€</option>  
         </select>
+        <select name="cocina">
+                <option value="1">Japonesa</option> 
+                <option value="2">China</option> 
+                <option value="3">Italiana</option>
+                <option value="10">Vegetariana</option> 
+                <option value="11">Vegana</option> 
+        </select>
+        <button type="submit" class="icono fa fa-search" ></button>
 </form>
