2e90f71126160ebd1a2908de23313096ba256b03
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-01 18:53:08 +0200

    conexion

--- www/conexion.php
+++ www/conexion.php
@@ -9,14 +9,14 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname,3306);
 
 if(!$conn ){
     die('Could not connect: ' . mysqli_error());
-    }
+}
     
-    mysqli_select_db($conn, 'restaurantes');
-    if (mysqli_select_db( $conn,'restaurantes' )) {
+mysqli_select_db($conn, 'restaurantes');
+if (mysqli_select_db( $conn,'restaurantes' )) {
         
-        } else {
-        echo "Error: " . mysqli_error($conn);
-        }
+} else {
+echo "Error: " . mysqli_error($conn);
+}
 
 
 ?>
