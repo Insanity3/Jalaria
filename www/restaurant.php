7b8a50676e10f2cdbd995062eb82139e90194c6c
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-02 14:10:11 +0200

    maps

--- www/restaurant.php
+++ www/restaurant.php
@@ -17,5 +17,4 @@
             <iframe src="<?php echo $row['maps']?>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
     </div>           
-
 <?php require 'componentes/footer.php'?>
