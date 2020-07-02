91c45969a1ef20302ae3a405aae6d6a67129a95f
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-02 10:14:51 +0200

    bootstrap

--- www/restaurant.php
+++ www/restaurant.php
@@ -1,17 +1,20 @@
 <?php require 'back/restaurantb.php';?>
 <?php require 'componentes/header.php'?>
-    <img src="<?php echo $foto ?>" alt="">
-    <h3><?php echo $row['nombre']?></h3>
-    <p><?php echo $row['localidad']?></p>
-    <p><?php echo $row['diceccion']?></p>
-    <p><?php echo $row['cp']?></p>
-    <p><?php echo $row['telf']?></p>
-    <p><?php echo $row['precios']?></p>
-    <p><?php echo $row['valoracion']?></p>
-    <p><?php echo $row['email']?></p>
-    <p><?php echo $row['web']?></p>
-    <p><?php echo $row['horario']?></p>
-    <p><?php echo $row['cocina']?></p>
-                
+    <div class="row">
+        <div class="col-md d-flex justify-content-center flex-column text-center align-items-center">
+            <img src="<?php echo $foto ?>" alt="" class="cabecera">
+            <h3><?php echo $row['nombre']?></h3>
+            <p><?php echo $row['localidad']?></p>
+            <p><?php echo $row['diceccion']?></p>
+            <p><?php echo $row['cp']?></p>
+            <p><?php echo $row['telf']?></p>
+            <p><?php echo $row['precios']?></p>
+            <p><?php echo $row['valoracion']?></p>
+            <p><?php echo $row['email']?></p>
+            <p><?php echo $row['web']?></p>
+            <p><?php echo $row['horario']?></p>
+            <p><?php echo $row['cocina']?></p>
+            </div>
+    </div>           
 
 <?php require 'componentes/footer.php'?>
