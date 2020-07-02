42f70cf82e47df0b9036446e0c830efb99db41f3
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-02 14:00:07 +0200

    mapa

--- www/restaurant.php
+++ www/restaurant.php
@@ -14,6 +14,7 @@
             <p><?php echo $row['web']?></p>
             <p><?php echo $row['horario']?></p>
             <p><?php echo $row['cocina']?></p>
+            <iframe src="<?php echo $row['maps']?>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
     </div>           
 
