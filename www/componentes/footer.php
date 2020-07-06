52f79a7904c552d5ddd797207a6d58d2911361af
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-06 12:48:27 +0200

    plantilla de bootstrap

--- www/componentes/footer.php
+++ www/componentes/footer.php
@@ -1,52 +1,36 @@
-<!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4">
+<!-- ======= Footer ======= -->
+<footer id="footer">
+        <div class="container">
+        <h3>Jalaria</h3>
+        <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+        <div class="social-links">
+            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+        <div class="copyright">
+            &copy; Copyright <strong><span>Jalaria</span></strong>. All Rights Reserved
+        </div>
+        
+        </div>
+    </footer><!-- End Footer -->
 
-<!-- Footer Elements -->
-    <div class="container">
+    <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-    <!-- Social buttons -->
-    <ul class="list-unstyled list-inline text-center">
-        <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-            <i class="fab fa-facebook-f"> </i>
-        </a>
-        </li>
-        <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-            <i class="fab fa-twitter"> </i>
-        </a>
-        </li>
-        <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-            <i class="fab fa-google-plus-g"> </i>
-        </a>
-        </li>
-        <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-            <i class="fab fa-linkedin-in"> </i>
-        </a>
-        </li>
-        <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
-            <i class="fab fa-dribbble"> </i>
-        </a>
-        </li>
-    </ul>
-    <!-- Social buttons -->
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 
-    </div>
-<!-- Footer Elements -->
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
 
-<!-- Copyright -->
-<div class="footer-copyright text-center py-3">© 2020 Copyright:
-<p>Cristina Oreja Zafra</p>
-</div>
-<!-- Copyright -->
-
-</footer>
-<!-- Footer -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- www/componentes/header.php
+++ www/componentes/header.php
@@ -32,55 +32,107 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     
     <!-- css -->
+    <link href="styles.css" rel="stylesheet">
     <!-- <link rel="stylesheet" href="styles.css" type="text/css"> -->
     
 </head>
 <body>
 
 <?php require 'back/indexb.php'?>
-    <header>
-    
-    <div class="row">
-        <div class="logo"></div>
-        <div class="col-md d-flex justify-content-center align-items-center">
-            <h1>Jalaria   </h1>.
-        </div>
-    </div>
-        
 
-        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbarsExample03">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="contacto.php">Contacto</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="nosotros.php">Sobre nosotros</a>
-                    </li>
-                    
-                    <?php if(isset($_SESSION['usuario'])){?>
+    <!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex align-items-center">
+
+    <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.php"><span>Jalaria</span></a></h1>
+      </div>
+
+        <nav class="nav-menu d-none d-lg-block">
+            <ul>
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="nosotros.php">Sobre nosotros</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+            <?php if(isset($_SESSION['usuario'])){?>
                         <li class="nav-item">
                             <a class="nav-link" href="">
                             <?php echo $_SESSION['usuario'];?>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="back/cerrar.php">
-                            Cerrar sesión
-                            </a>
-                        </li>
+                        <li class="book-a-table text-center"><a href="cerrar.php">Log out</a></li>
                         <?php }else{?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.php">
-                            Log In
-                            </a>
-                        </li>
+                            <li class="book-a-table text-center"><a href="login.php">Log in</a></li>
                     <?php }?>
-                </ul>
             
+            </ul>
+        </nav><!-- .nav-menu -->
+
+        </div>
+</header><!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background: url(assets/img/slide/slide-1.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <a href="index.php"><img src="img/logos/9ca707d3-343f-48f6-8f7e-909255b59985_200x200.png" alt="" class="img-fluid"></a>
+                <!-- <div>
+                  <a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
+                  <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
+                </div> -->
+              </div>
             </div>
-        </nav>
-    </header>
+          </div>
+
+          <!-- Slide 2 -->
+          <div class="carousel-item" style="background: url(assets/img/slide/slide-2.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                <!-- <div>
+                  <a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
+                  <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          <!-- Slide 3 -->
+          <div class="carousel-item" style="background: url(assets/img/slide/slide-3.jpg);">
+            <div class="carousel-background"><img src="assets/img/slide/slide-3.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
+                <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                <!-- <div>
+                  <a href="#menu" class="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
+                  <a href="#book-a-table" class="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon icofont-simple-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon icofont-simple-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+    </div>
+</section><!-- End Hero -->
