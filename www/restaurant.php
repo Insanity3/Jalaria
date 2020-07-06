605918f8897ddefae668ff3a1c3bbbf92ef1d3a5
Cristina Oreja Zafra <coreja@cifpfbmoll.eu>
2020-07-06 12:49:18 +0200

    restaurant

--- www/restaurant.php
+++ www/restaurant.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <?php require 'back/restaurantb.php';?>
 <?php require 'componentes/header.php'?>
     <div class="row">
@@ -15,6 +16,45 @@
             <p><?php echo $row['horario']?></p>
             <p><?php echo $row['cocina']?></p>
             <iframe src="<?php echo $row['maps']?>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            
+            <!-- Formulario de contacto -->
+
+        
+
+            <!-- Formulario opinion -->
+                <?php 
+                if(isset($_SESSION['usuario'])){
+                ?>
+                    <div>
+                    <h3>Deja tu valoración!</h3>
+                        <form action="" method="POST" name="opinion">
+                            <select name="valoracion" id="">
+                                <option value=""></option>
+                                <option value="1">1 estrella</option> 
+                                <option value="2">2 estrellas</option> 
+                                <option value="3">3 estrellas</option>
+                                <option value="4">4 estrellas</option> 
+                                <option value="5">5 estrellas</option>
+                            </select>
+                            <textarea name="opinion" id="" >
+                            </textarea>
+                        </form>
+                    </div>
+                <?php
+                }
+                ?>
+                <!-- Formulario de reservas -->
+                <h3>Reserva!</h3>
+                <form action="" method="POST" name="reserva">
+                    <input type="text" name="nombre">
+                    <input type="date" name="dia" step="1" min="2020-01-01" mas="2022-01-01">
+                    <input type="time" name="hora">
+                    <textarea name="comensales"></textarea>
+                </form>
+
+
             </div>
-    </div>           
+    </div>    
+
+
 <?php require 'componentes/footer.php'?>
